472517eb626dbd02e57ccd9b3fa23fb787790f5f
JP Hebert <JPHebert32@gmail.com>
2020-06-19 19:04:34 -0600

    notebook changes

--- Queries.sql
+++ Queries.sql
@@ -1,15 +1,8 @@
--- DROP TABLES
-DROP TABLE counties;
-DROP TABLE drought;
-DROP TABLE fires;
---DROP TABLE search_keywords;
-
 
 -- CHECK TABLES
-SELECT * FROM counties WHERE id = 55;
+SELECT * FROM counties;
 SELECT * FROM drought;
 SELECT * FROM fires;
---SELECT * FROM search_keywords;
 
 ------------------------------------------------------------------------------------------------
 -- 1. In descending order, list the frequency of fires by county
@@ -31,7 +24,7 @@ SELECT * FROM fires_count_counties;
 DROP VIEW riverside_fires;
 
 CREATE VIEW riverside_fires AS
-SELECT f.county, f.name, f.started, f.extinguished, f.acres_burned, f.search_keywords
+SELECT f.county, f.name, f.started, f.extinguished, f.acres_burned
 FROM fires f
 WHERE f.county = 'Riverside'
 ORDER BY f.started DESC;
@@ -42,14 +35,16 @@ SELECT * FROM riverside_fires;
 -- 3. Fires that have burned more than 10000 acres
 ----------------------------------------------------------------------------------------------
 
-DROP acres_burned;
+DROP VIEW acres_burned;
 
 CREATE VIEW acres_burned AS
-SELECT f.name, f.started, f.county, drought 
+SELECT f.name,f.acres_burned, f.started, f.extinguished, d.county, d.valid_start, d.valid_end, d.d4, d.d3, d.d2, d.d1, d.d0, d.no_drought
 FROM ((fires f
-INNER JOIN drought d
+Inner JOIN drought d
 	   ON f.county = d.county))
-WHERE f.acres_burned > 10000 
-ORDER BY f.acres_burned DESC;
+WHERE f.acres_burned > 10000;
 
-SELECT * FROM acres_burned;
+SELECT * 
+FROM acres_burned 
+WHERE valid_start >= '2013/07/15'
+AND  valid_end <= '2013/07/30';
--- Tables.sql
+++ Tables.sql
@@ -4,16 +4,13 @@
 DROP TABLE counties;
 DROP TABLE drought;
 DROP TABLE fires;
-DROP TABLE search_keywords;
 
 -- CREATE TABLES
 
 CREATE TABLE counties (
 	id SERIAL UNIQUE, 
 	county VARCHAR (255) NOT NULL,
-	CONSTRAINT pk_counties PRIMARY KEY (
-        county
-     )
+	CONSTRAINT pk_counties PRIMARY KEY (county)
 );
 
 CREATE TABLE drought (
@@ -30,6 +27,7 @@ CREATE TABLE drought (
 );
 
 CREATE TABLE fires (
+	id SERIAL UNIQUE, 	
 	acres_burned INT,
 	active BOOLEAN,
 	admin_unit VARCHAR(255),
@@ -38,9 +36,9 @@ CREATE TABLE fires (
 	crews_involved INT,
 	dozers INT,
 	engines INT,
-	extinguished DATE NOT NULL,
+	extinguished DATE,
 	fatalities INT,
-	fuel_type INT,
+	fuel_type VARCHAR(255),
 	helicopters INT, 
 	injuries INT,
 	latitude FLOAT(6), 
@@ -49,23 +47,18 @@ CREATE TABLE fires (
 	major_incident BOOLEAN,
 	name VARCHAR(255),
 	personnel_involved  INT,
-	search_description TEXT,
-	search_keywords TEXT,
 	started DATE NOT NULL,
 	structures_damaged INT,
 	structures_destroyed INT,
 	structures_threatened INT,
 	water_tenders INT,
+	CONSTRAINT pk_fires PRIMARY KEY (id),
 	FOREIGN KEY (county) REFERENCES counties(county)
 );
 
-CREATE TABLE search_keywords (
-	search_keywords TEXT,
-	FOREIGN KEY (name) REFERENCES fires(name)
-);
 
 -- CHECK TABLES
 SELECT * FROM counties;
 SELECT * FROM drought;
 SELECT * FROM fires;
-SELECT * FROM search_keywords;
+
